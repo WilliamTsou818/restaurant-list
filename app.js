af8926410382a091fe6cda7502357336971ca815
WilliamTsou818 <cccc1589@gmail.com>
2021-08-02 21:54:21 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 const app = express()
 const PORT = 3000
@@ -24,6 +25,13 @@ const routes = require('./routes')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
+
+// session
+app.use(session({
+  secret: 'MyRestaurantSecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(routes)
 
