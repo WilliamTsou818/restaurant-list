aeb362bd1dd270b3d1a178df07c806e495d21387
WilliamTsou818 <cccc1589@gmail.com>
2021-08-02 22:52:12 +0800

    feat: add auth route module for fackbook login feature

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,10 @@ const { authenticator } = require('../middleware/auth')
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
 
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/restaurants', authenticator, restaurants)
 router.use('/', authenticator, home)
 
